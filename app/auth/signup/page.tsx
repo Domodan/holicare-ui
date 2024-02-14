45daf81aff3d36f1e5589244f21ad52ea45760da
Domodan <ongomdaniel9@gmail.com>
2024-02-14 15:53:59 +0300

    finalized the auth UI

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -318,7 +318,7 @@ const SignUp: React.FC = () => {
                     className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white transition hover:bg-opacity-90"
                   />
                 </div>
-                {/* 
+
                 <button className="flex w-full items-center justify-center gap-3.5 rounded-lg border border-stroke bg-gray p-4 hover:bg-opacity-50 dark:border-strokedark dark:bg-meta-4 dark:hover:bg-opacity-50">
                   <span>
                     <svg
@@ -364,7 +364,6 @@ const SignUp: React.FC = () => {
                     </Link>
                   </p>
                 </div>
-                 */}
               </form>
             </div>
           </div>
